4c81d25d7fc6fe713797892a5edbf2ad24c7e4ce
Thiago Buarque <thiago.buarque@liferay.com>
2025-06-27 11:21:55 -0300

    simplify SecurityException

--- src/main/java/com/evry/analytics/security/exception/SecurityException.java
+++ src/main/java/com/evry/analytics/security/exception/SecurityException.java
@@ -1,26 +1,22 @@
 package com.evry.analytics.security.exception;
 
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
+
+import java.io.Serial;
 
 public class SecurityException extends RuntimeException {
 
+    @Serial
     private static final long serialVersionUID = 1L;
 
-    private final String message;
+    @Getter
     private final HttpStatus httpStatus;
 
     public SecurityException(String message, HttpStatus httpStatus) {
-        this.message = message;
+        super(message);
+
         this.httpStatus = httpStatus;
-    }
-
-    @Override
-    public String getMessage() {
-        return message;
-    }
-
-    public HttpStatus getHttpStatus() {
-        return httpStatus;
     }
 
 }
